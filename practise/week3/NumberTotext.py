ad1e229ffdf300f16276f7d22fe4023a28d8ee77
PhuongAnh2605 <anphuong2605@gmail.com>
2020-09-28 10:47:35 +0700

    Number To Text

--- practise/week3/NumberTotext.py
+++ practise/week3/NumberTotext.py
@@ -1,55 +1,109 @@
+# Dictonary of number has 3 group: units(from 1 to 9),
+# teens(from 11 to 19) and tens(from 10 to 90)
 number_dic = {
-    "mot": 1,
-    "hai": 2,
-    "ba": 3,
-    "bon": 4,
-    "nam": 5,
-    "sau": 6,
-    "bay": 7,
-    "tam": 8,
-    "chin": 9
+    1:"mốt",
+    2: "hai",
+    3: "ba",
+    4: "bốn",
+    5: "năm",
+    6: "sáu",
+    7: "bảy",
+    8: "tám",
+    9: "chín",
+
+    11: "mười một",
+    12: "mười hai",
+    13: "mười ba",
+    14: "mười bốn",
+    15: "mười năm",
+    16: "mười sáu",
+    17: "mười bảy",
+    18: "mười tám",
+    19: "mười chín",
+
+    10: "mười",
+    20: "hai mươi",
+    30: "ba mươi",
+    40: "bốn mươi",
+    50: "năm mươi",
+    60: "sáu mươi",
+    70: "bảy mươi",
+    80: "tám mươi",
+    90: "chín mươi"
 }
-number = input("Enter a string number: ")
-list1 = []
-list2 = []
-list3 = []
-list = []
-list4 = []
-list5 = []
-b = 0
-len_num = len(number)
-len_num = len_num // 3
-left = len_num % 3
+# Store the level of number from thousand to trillion
+thousand = 1000
+million = thousand * 1000
+billion = million * 1000
+trillion = billion * 1000
 
-#separate number
-for i in range(len_num+1) :
-    if 3*(i+1) <= len_num * 3 :
-        num1 = number[3*i : 3*i+3]
-        list.append(num1)
-        num1 = 0
 
-#first number
-for n in range(left) :
-    for m in number_dic :
-        if number[n] == m :
-            list1.append(number_dic[m])
-#3*number
-for i in range(len(list)):
-    for j in list[i] :
-        for m in number_dic :
-            if j == m :
-                list4.append(number_dic[m])
-#read
-if left == 1:
-    print(list1[0], ' ', list1[1], "ngan", end='')
-elif left == 2:
-    print(list1[0], ' ', list1[1], "trieu", end='')
-elif  left == 3:
-    print(list1[0], ' ', list1[1], "ti", end='')
-for h in range(len(list4)):
-    if 3*h <= len(list4) :
-        list5.append(list4[3*h:3*h+3])
-    if len_num == 3:
-        print(list[0], " tram ", list[1], " muoi ", list[2])
-    if len_num == 6:
-        print(list[0], " tram ", list[1], " muoi ", list[2], " ngan ", list[3], " tram ", list[4], " muoi ", list[5])
+# Main function
+def main():
+    # Ask user enter number separated by ','
+    number = input("Enter a string of number separated by (','): ")
+    # Split number by ','
+    num_list = number.split(',')
+    str = ''
+    # Append number to local variable str with no separator
+    for n in num_list:
+        str += n
+    number = int(str)
+    # Print number in text
+    print(num_text(number), 'đồng chẵn.')
+
+
+# Function num_text converts number to text and returns
+def num_text(num):
+    # If num less than 20 then return the value accordingly in num_dic
+    if num < 20:
+        return number_dic[num]
+    # If num less than 100 and if num divided by 10 return value in num_dic
+    # else find the units and return
+    if num < 100:
+        if num % 10 == 0 :
+            return number_dic[num]
+        else:
+            return number_dic[num // 10 * 10] + ' ' + number_dic[num%10]
+    # If num less than thousand and num divided by 100 then return value
+    # and add 'tram' at the end
+    # else find units by recursive
+    if num < thousand:
+        if num % 100 == 0 :
+            return number_dic[num // 100] + ' trăm'
+        else:
+            return number_dic[num // 100] + ' trăm ' + num_text(num % 100)
+    # If num less than million and num divided by 1000 then return value
+    # and add 'nagn' at the end
+    # else find units by recursive
+    if num < million :
+        if num % 1000 == 0 :
+            return num_text(num // thousand) + ' ngàn'
+        else :
+            return num_text(num // thousand) + ' ngàn ' + num_text(num % thousand)
+    # If num less than billion and num divided by million then return value
+    # and add 'trieu' at the end
+    # else find units by recursive
+    if num < billion :
+        if num % thousand == 0:
+            return num_text(num // million)+ ' triệu'
+        else :
+            return num_text(num // million) + ' triệu ' + num_text(num % million)
+    # If num less than trillion and num divided by billion then return value
+    # and add 'ti' at the end
+    # else find units by recursive
+    if num < trillion:
+        if num % billion == 0:
+            return num_text(num // billion) + ' tỉ'
+        else :
+            return num_text(num // billion) + ' tỉ ' + num_text(num % billion)
+    # If num less than trillion and num divided by trillion then return value
+    # and add 'nghin ti' at the end
+    # else find units by recursive
+    if num % trillion == 0:
+        return num_text(num // trillion) + ' nghìn tỉ'
+    else:
+        return num_text(num//trillion) + ' nghìn tỉ' + num_text(num % trillion)
+
+
+main()
