a74ecf5bf5487937f81f7de7bbb2703fef540a4e
PhuongAnh2605 <anphuong2605@gmail.com>
2020-10-03 21:38:30 +0700

    hw3

---
+++ homework/hw3/pr2_BubbleSort.py
@@ -0,0 +1,19 @@
+def main():
+    arr = [3, 5, 23, 56, 78, 9]
+    bubbleSort(arr)
+    print(arr)
+
+
+def bubbleSort(arr):
+    l = len(arr)
+    # Traverse through all array elements
+    for i in range(l-1):
+        # Last elements are already in place
+        # Traverse array from 0 to l-i-1
+        for j in range(l-i-1):
+            # Swap if the element found is greater than the next element
+            if arr[j] > arr[j+1]:
+                arr[j], arr[j+1] = arr[j+1], arr[j]
+
+
+main()
